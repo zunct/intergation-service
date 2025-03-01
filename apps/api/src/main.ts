e48d8a82aeaa1264bf4c348d7a83184da3e0633c
DungCT <dung@developer.legodesk.com>
2025-03-02 04:54:09 +0700

    feat(api): enhance API setup with Swagger documentation and global validation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Intergration service API')
+    .setDescription('OpenAPI documentation for the Intergration service API')
+    .setVersion('1.0')
+    .addTag('intergration')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -18,7 +18,7 @@ export const config = [
       turbo: turboPlugin,
     },
     rules: {
-      "turbo/no-undeclared-env-vars": "warn",
+      "turbo/no-undeclared-env-vars": "off",
     },
   },
   {
